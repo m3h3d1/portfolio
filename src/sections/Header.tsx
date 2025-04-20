d9a9a52b5782807700411be3f8000f3b33efa082
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2025-04-20 21:24:21 +0600

    Header Buttons Clickable

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,29 @@
+"use client";
+
+import { useState } from "react";
+
 export const Header = () => {
+  const [activeSection, setActiveSection] = useState("home");
+
+  const handleClick = (sectionId: string) => {
+    setActiveSection(sectionId);
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">
-          Home
-        </a>
-        <a href="#" className="nav-item">Projects</a>
-        <a href="#" className="nav-item">About</a>
-        <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+        {["home", "projects", "about", "contact"].map((section) => (
+          <button
+            key={section}
+            className={`nav-item ${
+              activeSection === section ? "bg-white text-gray-900" : ""
+            }`}
+            onClick={() => handleClick(section)}
+          >
+            {section.charAt(0).toUpperCase() + section.slice(1)}
+          </button>
+        ))}
       </nav>
     </div>
   );
