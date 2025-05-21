4d89320a528b39559238b25c82701be8bb19a866
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2025-05-21 23:51:16 +0600

    Contact me option edited

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -23,10 +23,13 @@ export const ContactSection = () => {
               </p>
             </div>
             <div>
-              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+              <a
+                href="mailto:mehedi.hasan.sde@gmail.com"
+                className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900 hover:bg-gray-800 transition-colors"
+              >
                 <span className="font-semibold">Contact Me</span>
                 <ArrowUpRightIcon className="size-4" />
-              </button>
+              </a>
             </div>
           </div>
         </div>
